8719ad08cecf5a79b1487944bab57ba5d71e8728
Ilias Stamatis <stamatis.iliass@gmail.com>
2012-02-15 20:14:30 +0200

    initial database.py commit

---
+++ address_book/database.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+#
+# Copyright (C) 2012 Ilias Stamatis <stamatis.iliass@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sqlite3
+
+class Database:
+    def __init__(self):
+        self.connection = sqlite3.connect("test.db")
+        self.cursor = self.connection.cursor()
+        self.create_tables()
+
+    def create_tables(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users(
+            name TEXT PRIMARY KEY
+            )''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS categories(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            user TEXT REFERENCES users(name)
+            ON UPDATE CASCADE ON DELETE CASCADE
+            )''')
+
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS contacts(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            surname TEXT,
+            mail TEXT,
+            address TEXT,
+            telephone INTEGER,
+            category INTEGER REFERENCES categories(id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+            )''')
+
+    def commit(self):
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
